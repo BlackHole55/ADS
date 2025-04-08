ea6feb5f3cdee1c4c802babb2b66bba0ef413ce2
Meirhan Syzdykov <syzdykovmr@gmail.com>
2025-04-09 00:24:09 +0500

    feat: Add MyLinkedList implementation

---
+++ Assigment2/MyLinkedList.java
@@ -0,0 +1,58 @@
+package Assigment2;
+
+public class MyLinkedList<T> implements MyList<T> {
+    private MyNode head;
+    private MyNode tail;
+    private int size;
+    
+    public MyLinkedList() {}
+
+    // Size getter
+    public int size() {
+        return size;
+    }
+
+    // Add element to linked list
+    public void add(T data) {
+        MyNode newNode = new MyNode(data);
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        size++;
+    }
+
+    // Get element by index
+    public T get(int index) {
+        checkIndex(index);
+        MyNode current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+
+        return (T) current.data;
+    }
+
+    // Remove element by index
+    public void remove(int index) {
+
+    }
+
+    private static class MyNode {
+        Object data;
+        MyNode next;
+        MyNode prev;
+
+        MyNode(Object data) {
+            this.data = data;
+        }
+    }
+
+    private void checkIndex(int index){
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + " not found");
+        }
+    }
+}
